7914445fc4cccfc1bf51a79b5754160e5dde6cc7
Esteban Loaiza <esteban.loaizaa@gmail.com>
2021-11-30 14:47:03 -0300

    gh pages

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ class App extends Component {
       monster.name.toLowerCase().includes(searchField.toLocaleLowerCase()));
     return (
       <div className="App">
+      <h1>Monstruos</h1>
         <SearchBox
           placeholder='search monster'
           handleChannge={this.handleChange}
